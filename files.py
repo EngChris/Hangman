6f49c805f0b92fb37ed17530ed5e796bf68c4800
Chris <chrissmaina@gmail.com>
2024-09-05 18:38:15 +0300

    working on integration
    Integrating several files into our main file

--- files.py
+++ files.py
@@ -9,8 +9,10 @@ def read_file(file_name):
     f = open("words.txt", "rt")
     data = f.read()
     print(data)
+    word=[]
     word = data.split("\n")
     print(word)
+
     for x in word:
            
     #print(x.strip('\n'), end=" ")
@@ -25,5 +27,15 @@ def read_random(file_name):
     print(word2)
     f.close()
     
+    
+def print_random(file_name):
+    f= open(file_name)
+    data= f.read()
+    word = data.split("\n")
+    rand= random.choice(word)
+    print(rand)
+    
 file = "words.txt"
 read_file(file)
+#read_random(file)
+#print_random(file)
